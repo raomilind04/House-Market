fe57ba9f1198af76cc68508cd6ab59220c66a5f3
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2022-07-30 18:00:11 +0530

    add sign up feature

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 function SignUP() {
   const [showPassword, setShowPassword] = useState(false);
@@ -18,9 +25,31 @@ function SignUP() {
     setFormData((prevState) => {
       return {
         ...prevState,
-        [e.target.id]: [e.target.value],
+        [e.target.id]: e.target.value,
       };
     });
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      const formDataCopy= {...formData}; 
+      delete formDataCopy.password; 
+      formDataCopy.timestamp= serverTimestamp(); 
+      await setDoc(doc(db, "users", user.uid), formDataCopy)
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,7 +58,7 @@ function SignUP() {
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="text"
             className="nameInput"
@@ -64,7 +93,7 @@ function SignUP() {
               }}
             />
           </div>
-         
+
           <div className="signUpBar">
             <p className="signUpText">Sign Up</p>
             <button className="signUpButton">
